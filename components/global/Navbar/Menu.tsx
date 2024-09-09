a977bb0e30d5ca203fc6a9134fc19a5ac3a1e76b
Miguel Stevens <miguel@studiomonty.be>
2024-09-09 20:34:31 +0200

    Sitemap and active link

--- components/global/Navbar/Menu.tsx
+++ components/global/Navbar/Menu.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link'
 import { Fragment, useState } from 'react'
+import { usePathname } from 'next/navigation'
 
 interface MenuProps {
   items: any[]
@@ -18,29 +19,46 @@ const Menu = () => {
 export default Menu
 
 const DeskTopMenu = () => {
+  const currentPath = usePathname()
+
   return (
     <Fragment>
       <nav className="main-menu d-none d-xl-block">
-        <ul>
+        <ul className="underline-offset-4">
           <li className="menu-item">
-            <Link href="/">Home</Link>
-          </li>
-          <li className="menu-item">
-            <Link href="/tours">
-              <span className="main-btn secondary-btn !pl-0 px-3 py-2">All Tours</span>
+            <Link
+              href="/"
+              style={{
+                textDecoration: currentPath === '/' ? 'underline' : 'none',
+              }}
+            >
+              Home
             </Link>
           </li>
           <li className="menu-item">
-            <Link href="/west-tour-madeira">West Tour</Link>
+            <Link href="/tours">
+              <span
+                className="main-btn secondary-btn !pl-0 px-3 py-2"
+                style={{
+                  textDecoration:
+                    currentPath === '/tours' ? 'underline' : 'none',
+                }}
+              >
+                All Tours
+              </span>
+            </Link>
           </li>
           <li className="menu-item">
-            <Link href="/wine-tour-madeira">Wine Tour</Link>
+            <Link href="/west-tour-madeira" style={{ textDecoration: currentPath === '/west-tour-madeira' ? 'underline' : 'none' }}>West Tour</Link>
           </li>
           <li className="menu-item">
-            <Link href="/rock-climbing-madeira">Rock Climbing Tour</Link>
+            <Link href="/wine-tour-madeira" style={{ textDecoration: currentPath === '/wine-tour-madeira' ? 'underline' : 'none' }}>Wine Tour</Link>
           </li>
           <li className="menu-item">
-            <Link href="/east-tour-madeira">East Tour</Link>
+            <Link href="/rock-climbing-madeira" style={{ textDecoration: currentPath === '/rock-climbing-madeira' ? 'underline' : 'none' }}>Rock Climbing Tour</Link>
+          </li>
+          <li className="menu-item">
+            <Link href="/east-tour-madeira" style={{ textDecoration: currentPath === '/east-tour-madeira' ? 'underline' : 'none' }}>East Tour</Link>
           </li>
           {/*<li className="menu-item">*/}
           {/*  <Link href="/climb-pico-das-torres">Pico Das Torres</Link>*/}
